246bb8105b0f8318946b875e4cd3102076d487fb
cat <lotteng_@naver.com>
2023-01-17 16:43:41 +0900

    Add Form(Login), Objects rename

---
+++ cosetTest/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace cosetTest
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPw = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblPw = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.ImageLocation = "\\\\E_Doc\\98 ETC\\E03 소모품(데이터베이스)\\test\\사진\\logo(360x140).png";
+            this.imgLogo.Location = new System.Drawing.Point(54, 51);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(180, 70);
+            this.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgLogo.TabIndex = 0;
+            this.imgLogo.TabStop = false;
+            // 
+            // txtId
+            // 
+            this.txtId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtId.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtId.Location = new System.Drawing.Point(69, 169);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(165, 18);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtPw
+            // 
+            this.txtPw.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPw.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPw.Location = new System.Drawing.Point(69, 205);
+            this.txtPw.Name = "txtPw";
+            this.txtPw.Size = new System.Drawing.Size(165, 18);
+            this.txtPw.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Window;
+            this.btnLogin.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLogin.Location = new System.Drawing.Point(54, 251);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(180, 39);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(43, 171);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 12);
+            this.lblId.TabIndex = 4;
+            this.lblId.Text = "ID";
+            // 
+            // lblPw
+            // 
+            this.lblPw.AutoSize = true;
+            this.lblPw.Location = new System.Drawing.Point(36, 207);
+            this.lblPw.Name = "lblPw";
+            this.lblPw.Size = new System.Drawing.Size(23, 12);
+            this.lblPw.TabIndex = 5;
+            this.lblPw.Text = "PW";
+            // 
+            // Login
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.lblPw);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPw);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.imgLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgLogo;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPw;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblPw;
+    }
+}
+
